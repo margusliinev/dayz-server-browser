febd53c7e94d775e315187f2d75c1918ac572f96
Margus Liinev <margus.liinev@gmail.com>
2025-07-27 12:09:46 +0300

    update host config

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ const start = async () => {
     try {
         await runMigrations();
         const PORT = parseInt(process.env.PORT || '3000');
+        const HOST = process.env.NODE_ENV === 'production' ? '::' : '127.0.0.1';
 
         const app = await buildApp();
-        await app.listen({ port: PORT, host: '127.0.0.1' });
+        await app.listen({ port: PORT, host: HOST });
     } catch (err) {
         console.error(err);
         process.exit(1);
